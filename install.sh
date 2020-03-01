2169aec7c101532ea878476cbb2d1fd177e7f7ce
Dawid Czarnik <dcz31249@gmail.com>
2020-03-02 00:05:38 +0100

    update

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
-sudo pacman --noconfirm -S xorg-server xorg-xinit i3 mesa vlc alacritty feh dunst
+sudo pacman --noconfirm -S xorg-server xorg-xinit i3 mesa vlc alacritty feh dunst i3lock-color --ignore i3lock
 cp ./.bashrc ~/.bashrc
 source ~/.bashrc
 cp ./xinitrc ~/.xinitrc
 cp ./i3.config ~/.config/i3/config
-if [ ! -d "~/.config/i3status"] ; then
+if [ ! -d "~/.config/i3status" ] ; then
 	mkdir ~/.config/i3status
 fi
 cp ./i3status.config ~/.config/i3status/config
