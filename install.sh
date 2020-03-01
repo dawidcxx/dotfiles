79d53d099b5d5e11151ff8cb98df31239a822ee6
Dawid Czarnik <dcz31249@gmail.com>
2020-03-01 23:41:48 +0100

    update

--- install.sh
+++ install.sh
@@ -2,3 +2,5 @@ sudo pacman --noconfirm -S xorg-server xorg-xinit i3 mesa vlc alacritty feh
 cp ./.bashrc ~/.bashrc
 source ~/.bashrc
 cp ./xinitrc ~/.xinitrc
+cp ./i3.config ~/.config/i3/config
+cp ./i3status.config ~/.config/i3status/config
